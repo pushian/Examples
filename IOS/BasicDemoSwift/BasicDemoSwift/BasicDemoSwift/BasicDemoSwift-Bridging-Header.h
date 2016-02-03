f4915686c2c39234e7795a9f3acead591b384d4f
Reynes Martial <contact@thewriterzone.com>
2016-02-03 18:04:41 +0100

    ios: BasicDemo Swift

---
+++ IOS/BasicDemoSwift/BasicDemoSwift/BasicDemoSwift/BasicDemoSwift-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  TestAdactiveSwift-Bridging-Header.h
+//  TestAdactiveSwift
+//
+//  Created by Openfield Mobility on 26/01/2016.
+//  Copyright © 2016 Openfield Mobility. All rights reserved.
+//
+
+#ifndef TestAdactiveSwift_Bridging_Header_h
+#define TestAdactiveSwift_Bridging_Header_h
+
+#import <ADSumMapViewController.h>
+
+#endif /* TestAdactiveSwift_Bridging_Header_h */
---
+++ IOS/BasicDemoSwift/BasicDemoSwift/BasicDemoSwift/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  BasicDemoSwift
+//
+//  Created by Reynes Martial on 01/02/2016.
+//  Copyright © 2016 adactive. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+
+class ViewController: UIViewController, ADSumMapViewControllerDelegate {
+    
+    var adSumMapViewController: ADSumMapViewController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.adSumMapViewController = ADSumMapViewController()
+        self.adSumMapViewController.AdactiveParentRect = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)
+        self.adSumMapViewController.delegate = self
+        self.adSumMapViewController.view.backgroundColor = UIColor.whiteColor()
+        self.view.backgroundColor = UIColor.whiteColor()
+        self.view.addSubview(self.adSumMapViewController.view)
+        
+        self.adSumMapViewController.update()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func dataDidFinishUpdating(adSumViewController: AnyObject!) {
+        self.adSumMapViewController.start()
+    }
+    
+    func dataDidFinishUpdating(adSumViewController: AnyObject!, withError error: NSError!) {
+        if(self.adSumMapViewController .isMapDataAvailable()) {
+            self.adSumMapViewController.start()
+        } else {
+            let alert = UIAlertController(title: "Update finished with errors", message: nil, preferredStyle: UIAlertControllerStyle.Alert)
+            let yesButton = UIAlertAction(title: "Ok", style: .Default, handler: nil)
+            alert.addAction(yesButton)
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+    
+    func mapDidFinishLoading(adSumViewController: AnyObject!) {
+        //self.adSumMapViewController.customizeInactivePlaces(UIColor.redColor())
+        self.adSumMapViewController.setCameraMode(CameraMode.init(0))
+        
+        self.adSumMapViewController.setCurrentFloor(2);
+        
+        self.adSumMapViewController.setZoom(0.1);
+        
+        //if([self.delegate respondsToSelector:@selector(adSumViewController:OnPOIClicked:placeId:)])
+        
+        let canWork = self.respondsToSelector(Selector("mapDidFinishLoading:"))
+        if canWork==false {
+            NSLog(" BUG ");
+        }
+        else {
+            NSLog(" OK");
+        }
+        
+        
+        //- (void)adSumViewController:(id)adSumViewController OnPOIClicked:(NSArray *)poiIds placeId:(long)placeId;
+        let canWork2 = self.respondsToSelector(Selector("adSumViewController:OnPOIClicked:placeId:"))
+            if canWork2==false {
+            NSLog(" BUG ");
+        }
+            else {
+            NSLog(" OK");
+        }
+
+    }
+ //   func OnPOIClicked(poiIds: [AnyObject]!, adSumViewController: AnyObject!, placeId: NSNumber!) {
+  //      NSLog(" EEEE");
+  //  }
+   /*func OnPOIClicked(poiIds: [AnyObject]!, placeId: NSNumber!, adSumViewController: AnyObject!) {
+    NSLog(" EEEE");
+}*/
+    
+    func OnPOIClicked(poiIds: [AnyObject]!, placeId: Int, adSumViewController: AnyObject!) {
+        NSLog(" EEEE");
+    }
+
+    /*
+    func adSumViewController(adSumViewController: AnyObject!, onPOIClicked poiIds: [AnyObject]!, placeId: AnyObject!) {
+        NSLog(" ahah");
+       // self.adSumMapViewController.unLightAll()
+       // self.adSumMapViewController.centerOnPlace(placeId)
+       // self.adSumMapViewController.highLightPlace(placeId, color: UIColor.greenColor())
+    }*/
+    /*
+    func adSumViewController(adSumViewController: AnyObject!, onPOIClicked poiIds: [AnyObject]!, placeId: Int) {
+        self.adSumMapViewController.unLightAll()
+        self.adSumMapViewController.centerOnPlace(placeId)
+        self.adSumMapViewController.highLightPlace(placeId, color: UIColor.greenColor())
+    }*/
+}
